bb199609de8aed10de7d6b8115f51b51755bafcd
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-09-27 16:06:53 +0200

    Change for loop with a for on solutions, with a pointer and add "tests"

--- main.cpp
+++ main.cpp
@@ -3,15 +3,16 @@
 
 using namespace std;
 
+// Get the winner of a given table
 int getWinner(int table[]) {
   int nbMatch = 0;
   int solutions[][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 4, 7},
                         {2, 5, 8}, {3, 6, 9}, {3, 5, 7}, {1, 5, 9}};
 
-  for (int j = 0; j < sizeof(solutions); j++) {
+  for (int *solution : solutions) {
     nbMatch = 0;
     for (int i = 0; i < 3; i++) {
-      if (table[solutions[j][i] - 1] == 0) {
+      if (table[solution[i] - 1] == 0) {
         nbMatch++;
       }
     }
@@ -26,7 +27,10 @@ int getWinner(int table[]) {
 }
 
 int main() {
-  int table[9] = {1, 1, 1, 0, 0, 0, 0, 0, 0};
+  //   int table[9] = {1, 1, 1, 0, 0, 0, 0, 0, 0}; // 1 is winner
+  //   int table[9] = {1, 1, 0, 0, 0, 0, 0, 0, 0}; // 0 is winner
+  //   int table[9] = {1, 0, 1, 1, 1, 0, 1, 0, 0}; // 1 is winner
+  int table[9] = {1, 0, 1, 1, 1, 0, 0, 1, 0}; // no one is winner
 
   cout << getWinner(table) << endl;
 }
